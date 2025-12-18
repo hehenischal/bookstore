f4fc8af5166e22538ed93b9887d5121dfb9afd33
hehenischal <nischallc56@gmail.com>
2025-12-18 16:01:35 +0545

    booknames

--- home/views.py
+++ home/views.py
@@ -4,12 +4,14 @@ from .models import Genre,Book,Publication,Author
 def index(request):
     genres = Genre.objects.all()
     books = Book.objects.all()
+    features = books[:3]
     publications = Publication.objects.all()
     authors = Author.objects.all()
     context = {
         'genres': genres,
         'books': books,
         'publications': publications,
-        'authors': authors
+        'authors': authors,
+        'features': features,
     }
     return render(request, 'index.html',context)
