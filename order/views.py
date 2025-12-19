059e6f1f1ecd9ae707c8daff9ff8a3e7036ff772
hehenischal <nischallc56@gmail.com>
2025-12-19 12:22:48 +0545

    (.)

--- order/views.py
+++ order/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Order, Transaction
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required
+def buy(request, order_id):
+    
+    return render(request, 'order/buy.html', {'order_id': order_id})
+
+
+def failure(request, order_id):
+    return render(request, 'order/failure.html', {'order_id': order_id})
+
+def success(request, order_id):
+    return render(request, 'order/success.html', {'order_id': order_id})
